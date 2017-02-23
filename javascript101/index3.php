10b3c0ff7019b8cb681dc1cfe4a60839df2d0a8a
Ricandid <ricandid@gmail.com>
2017-02-23 13:02:10 +0800

    Add files via upload

---
+++ javascript101/index3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head><title>Activity 5</title>
+<script language="javascript" type="text/javascript">
+<!--
+	function getConfirmation(){
+		var retVal=confirm("Do you want to confirm?")
+
+		if (retVal==true) {
+			document.write("You may proceed.")
+			return true
+		}
+		else{
+			document.write("See you next time")
+			return false
+		}
+	}
+-->
+</script>
+</head>
+<body>
+	<input type="button" value="Click Me!" onclick="getConfirmation()"/>
+</body>
+</html>
---
+++ javascript101/index4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head><title>Activity 6</title>
+<script language="javascript" type="text/javascript">
+<!--
+	function getName(){
+		var retVal=prompt("Enter your name: ", "your name here");
+			document.write("Hi, " +retVal);
+	}
+-->
+</script>
+</head>
+<body>
+<form>
+	<input type="button" value="Introduce yourself" onclick="getName()"/>
+</form>
+</body>
+</html>
